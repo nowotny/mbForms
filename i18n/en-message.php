bddbcd60d573e0e9450609741ed8a987ab9abd16
Michel Käser <mk@frontender.ch>
2013-04-18 19:44:27 +0200

    EN lang (fix missing string errors)

---
+++ i18n/en-message.php
@@ -0,0 +1,77 @@
+<?php
+
+    /**
+     * Polish file for plugin mbforms
+     *
+     * @package Plugins
+     * @subpackage mbforms
+     *
+     * @author Piotr Łukaszewski <nowotnypl@gmail.pl>
+     * @version Wolf 0.7.3
+     * @version mbForms 1.0.2
+     */
+
+    return array(
+	'<p>For support visit <a href="http://www.mikebarlow.co.uk">www.mikebarlow.co.uk</a>.<br />This was a hobby project so support isn\'t full time but I will endeavour to answer questions and update the script as much as possible!<br /><br />Feedback / Suggestions are more then welcome.</p>' => '<p>For support visit <a href="http://www.mikebarlow.co.uk">www.mikebarlow.co.uk</a>.<br />This was a hobby project so support isn\'t full time but I will endeavour to answer questions and update the script as much as possible!<br /><br />Feedback / Suggestions are more then welcome.</p>',
+	'Add Item' => 'Add Item',
+	'Are you sure?' => 'Are you sure?',
+	'Checkbox' => 'Checkbox',
+	'Create Form' => 'Create Form',
+	'Delete' => 'Delete',
+	'Delete Item' => 'Delete Item',
+	'Denotes required fields' => 'Denotes required fields',
+	'Displaying Your Forms' => 'Displaying Your Forms',
+	'Drop Down' => 'Drop Down',
+	'Edit' => 'Edit',
+	'Edit Form' => 'Edit Form',
+	'Email Results To' => 'Email Results To',
+	'Extras' => 'Extras',
+	'Failed to delete the form!' => 'Failed to delete the form!',
+	'Failed to save your form, please try again.' => 'Failed to save your form, please try again.',
+	'Failed to send your email! Please try again' => 'Failed to send your email! Please try again',
+	'For classes, JS events etc...' => 'For classes, JS events etc...',
+	'For drop downs and radio buttons. Seperate the options with a comma.' => 'For drop downs and radio buttons. Seperate the options with a comma.',
+	'Form Builder' => 'Form Builder',
+	'Form List' => 'Form List',
+	'Form Name' => 'Form Name',
+	'Form Settings' => 'Form Settings',
+	'Form has been deleted!' => 'Form has been deleted!',
+	'Form has been saved!' => 'Form has been saved!',
+	'Form has been saved, however <span style="text-decoration: underline">none</span> of the form fields have been saved.' => 'Form has been saved, however <span style="text-decoration: underline">none</span> of the form fields have been saved.',
+	'Form has been saved, however not all the form items have been saved.' => 'Form has been saved, however not all the form items have been saved.',
+	'Forms' => 'Forms',
+	'Input Box' => 'Input Box',
+	'Items' => 'Items',
+	'Label' => 'Label',
+	'Order' => 'Order',
+	'Please Select an Option' => 'Please Select an Option',
+	'Radio Buttons' => 'Radio Buttons',
+	'Required?' => 'Required?',
+	'Save Form' => 'Save Form',
+	'Save Settings' => 'Save Settings',
+	'Select Template' => 'Select Template',
+	'Settings have been saved!' => 'Settings have been saved!',
+	'Submission' => 'Submission',
+	'Submit Form' => 'Submit Form',
+	'Success Template' => 'Success Template',
+	'Text Area' => 'Text Area',
+	'There has been an email submission via :name. See below for submission details' => 'There has been an email submission via :name. See below for submission details',
+	'There were one or more errors with the data you entered!' => 'There were one or more errors with the data you entered!',
+	'To display your form, place the following code into the page you wish the form to appear on while substituting FORMNAME for the name of the your form.<br /><br />&lt;?php mbForm(\'FORMNAME\'); ?&gt;' => 'To display your form, place the following code into the page you wish the form to appear on while substituting FORMNAME for the name of the your form.<br /><br />&lt;?php mbForm(\'FORMNAME\'); ?&gt;',
+	'Type' => 'Type',
+	'Use Captcha' => 'Use Captcha',
+	'Values' => 'Values',
+	'You did not enter the correct captcha code, please try again!' => 'You did not enter the correct captcha code, please try again!',
+	'You do not have permission to access the requested page!' => 'You do not have permission to access the requested page!',
+	'You have not filled out ":label"' => 'You have not filled out ":label"',
+	'You have not yet created any forms' => 'You have not yet created any forms',
+	'You must add at least one form item.' => 'You must add at least one form item.',
+	'You must fill out a name for this form' => 'You must fill out a name for this form',
+	'You must fill out a recipient email address for the form.' => 'You must fill out a recipient email address for the form.',
+	'You must select a success template.' => 'You must select a success template.',
+	'You must select at least one item for the form.' => 'You must select at least one item for the form.',
+	'You\'ll need to enter the correct keys in these fields to be able to use mbForms captcha functionality. mbForms uses reCaptcha for it\'s captcha functionality, you can create your keys <a href=\'https://www.google.com/recaptcha/admin/create\'>here</a>' => 'You\'ll need to enter the correct keys in these fields to be able to use mbForms captcha functionality. mbForms uses reCaptcha for it\'s captcha functionality, you can create your keys <a href=\'https://www.google.com/recaptcha/admin/create\'>here</a>',
+	'mbForms Support' => 'mbForms Support',
+	'reCaptcha Private Key' => 'reCaptcha Private Key',
+	'reCaptcha Public Key' => 'reCaptcha Public Key',
+    );
